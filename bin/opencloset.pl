2e2e6833ebee8d17b5978ab7bf2dc917f3eb63c1
Hyungsuk Hong <aanoaa@gmail.com>
2013-10-23 04:13:13 +0900

    add webabb file

---
+++ bin/opencloset.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+
+plugin 'PODRenderer';
+
+get '/' => sub {
+    my $self = shift;
+    $self->render('index');
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Welcome';
+Welcome to the Mojolicious real-time web framework!
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
