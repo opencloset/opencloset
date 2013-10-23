25c3fd09317137e39a40c8dd43ab252526c23274
Hyungsuk Hong <aanoaa@gmail.com>
2013-10-23 16:46:44 +0900

    use twitter-bootstrap

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
 
-plugin 'PODRenderer';
+plugin 'haml_renderer';
 
 get '/' => sub {
     my $self = shift;
@@ -16,9 +16,32 @@ __DATA__
 % title 'Welcome';
 Welcome to the Mojolicious real-time web framework!
 
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
+@@ layouts/default.html.haml
+!!! 5
+%html{:lang => "en"}
+  %head
+    %meta{:charset => "utf-8"}/
+    %title Opencloset, sharing clothes
+    %meta{:content => "", :name => "description"}/
+    %meta{:content => "", :name => "author"}/
+    %link{:href => "/assets/css/bootstrap.css", :rel => "stylesheet"}/
+    %link{:href => "/assets/css/screen.css", :rel => "stylesheet"}/
+  %body
+    .container
+      .navbar
+        .navbar-inner
+          %a.brand{:href => "#"} OPEN-CLOSET
+          %ul.nav
+            %li
+              %a{:href => "#"} Home
+            %li
+              %a{:href => "#"} Search
+      .content
+        = content
+      %footer.footer
+        .container
+          %p
+            %a{:href => "#"} facebook
+          %p
+            %a{:href => "#"} twitter
+      %script{:src => "//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"}
---
+++ cpanfile
@@ -0,0 +1,7 @@
+requires 'DBIx::Class';
+requires 'DBIx::Class::EncodedColumn';
+requires 'Mojolicious::Lite';
+requires 'Mojolicious::Plugin::HamlRenderer';
+requires 'Mojolicious::Plugin::Validator';
+requires 'Plack';
+requires 'Plack::Middleware::ReverseProxy';
