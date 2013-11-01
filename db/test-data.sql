56acee5a47a65b13bd9a5ce57623b5b0b391ff0e
Hyungsuk Hong <aanoaa@gmail.com>
2013-11-01 11:15:19 +0900

    adjust test-data SQL

--- db/test-data.sql
+++ db/test-data.sql
@@ -1,21 +1,26 @@
 START TRANSACTION;
 INSERT INTO `donor` (`id`, `name`,`email`,`phone`,`comment`) VALUES (1,'한만일',NULL,NULL,'안녕~'),(2,'김소령',NULL,NULL,'(*-*)b');
-INSERT INTO `clothe` (`id`,`no`,`chest`,`waist`,`arm`,`pants_len`,`category_id`,`top_id`,`bottom_id`,`donor_id`,`status_id`) VALUES (1,'J00001', 94, 78, 51, NULL, 1, NULL, NULL, 1, 1), (2,'P00001', NULL, NULL, NULL, 102, 2, NULL, NULL, 1, 1);
+INSERT INTO `clothe` (`id`,`no`,`chest`,`waist`,`arm`,`pants_len`,`category_id`,`top_id`,`bottom_id`,`donor_id`,`status_id`) VALUES 
+(1,'Jck00001', 94, 78, 51, NULL, 1, NULL, NULL, 1, 1),
+(2,'Pts00001', NULL, NULL, NULL, 102, 2, NULL, NULL, 1, 1),
+(3,'Shr00001', NULL, NULL, NULL, NULL, 3, NULL, NULL, 1, 1),
+(4,'Sho00001', NULL, NULL, NULL, NULL, 4, NULL, NULL, 1, 1),
+(5,'Tie00001', NULL, NULL, NULL, NULL, 6, NULL, NULL, 1, 1);
 UPDATE `clothe` SET `bottom_id`=2 WHERE `id`=1;
 UPDATE `clothe` SET `top_id`=1 WHERE `id`=2;
 INSERT INTO `donor_clothe` (`donor_id`, `clothe_id`, `comment`, `donation_date`) VALUES (1, 1, '필요없어서 했습니다', NOW()), (1, 2, '', NOW());
 
 -- 대여중인거
-INSERT INTO `clothe` (`id`,`no`,`chest`,`waist`,`arm`,`pants_len`,`category_id`,`top_id`,`bottom_id`,`donor_id`,`status_id`) VALUES (3,'J00002', 99, 82, 55, NULL, 1, NULL, NULL, 1, 2), (4,'P00002', NULL, NULL, NULL, 112, 2, NULL, NULL, 1, 2);
-UPDATE `clothe` SET `bottom_id`=4 WHERE `id`=3;
-UPDATE `clothe` SET `top_id`=3 WHERE `id`=4;
+INSERT INTO `clothe` (`id`,`no`,`chest`,`waist`,`arm`,`pants_len`,`category_id`,`top_id`,`bottom_id`,`donor_id`,`status_id`) VALUES (6,'Jck00002', 99, 82, 55, NULL, 1, NULL, NULL, 1, 2), (7,'Pts00002', NULL, NULL, NULL, 112, 2, NULL, NULL, 1, 2);
+UPDATE `clothe` SET `bottom_id`=7 WHERE `id`=6;
+UPDATE `clothe` SET `top_id`=6 WHERE `id`=7;
 INSERT INTO `donor_clothe` (`donor_id`, `clothe_id`, `comment`, `donation_date`) VALUES (1, 3, '남아서..', NOW()), (1, 4, '', NOW());
 
 INSERT INTO `guest` (`id`,`name`,`email`,`phone`,`gender`,`address`,`age`,`purpose`,`chest`,`waist`,`arm`,`pants_len`,`height`,`weight`,`create_date`,`visit_date`) VALUES (1,'홍형석','aanoaa@gmail.com','01031820000',0,'서울시 동작구 사당동',32,'입사면접',93,78,51,102,168,59,'2013-01-03','2013-01-03');
 
 INSERT INTO `order` (`id`,`guest_id`,`status_id`,`rental_date`,`target_date`,`return_date`,`price`,`discount`,`comment`,`payment_method`) VALUES (1,1,2,'2013-10-18','2013-10-21',NULL,20000,0,NULL,'현금');
 
-INSERT INTO `clothe_order` (`clothe_id`,`order_id`) VALUES (3,1), (4,1);
+INSERT INTO `clothe_order` (`clothe_id`,`order_id`) VALUES (6,1), (7,1);
 
-INSERT INTO `satisfaction` (`guest_id`,`clothe_id`,`chest`,`waist`,`arm`,`top_fit`,`bottom_fit`,`create_date`) VALUES (1,3,1,2,3,4,5,'2013-10-18');
+INSERT INTO `satisfaction` (`guest_id`,`clothe_id`,`chest`,`waist`,`arm`,`top_fit`,`bottom_fit`,`create_date`) VALUES (1,6,1,2,3,4,5,'2013-10-18');
 COMMIT;
