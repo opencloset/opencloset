5c843476b09d6b39fcdd2596b0a7ad0e2ca3f0a1
Keedi Kim <keedi.k@gmail.com>
2013-11-05 11:04:51 +0900

    Support 'url' option for local user
    개발 장비에서 작업하는 사람들을 위해 url 옵션을 추가합니다.
    다음 명령을 이용해 주소를 지정할 수 있습니다.
    
    bin/opencloset-import-csv.pl --url=http://localhost:3000 data.csv

--- bin/opencloset-import-csv.pl
+++ bin/opencloset-import-csv.pl
@@ -5,34 +5,40 @@ use utf8;
 use strict;
 use warnings;
 
-use Getopt::Long;
+use Getopt::Long::Descriptive;
 use HTTP::Request::Common;
 use HTTP::Request;
 use JSON;
 use LWP::UserAgent;
-use Pod::Usage;
 use Text::CSV;
 
-my %options;
-GetOptions(\%options, "--help");
+my ( $opt, $usage ) = do {
+    my $URL = $ENV{OPENCLOSET_URL} || 'http://localhost:5000';
 
-run(\%options, @ARGV);
+    describe_options(
+        "%c %o [csv file]",
+        [ 'url=s',  "opencloset url (default: $URL)", { default => $URL } ],
+        [],
+        [ 'help|h', 'print usage message and exit' ],
+    );
+};
 
-sub trim { shift =~ s/(^ +| +$)//gr }
+run( $opt, @ARGV );
 
 sub run {
-    my($opts, @args) = @_;
+    my ( $opt, @args ) = @_;
 
-    pod2usage(0) if $opts->{help} || @args < 1;
+    print($usage->text), exit                        if     $opt->help;
+    print($usage->text), die("csv file is needed\n") unless @args >= 1;
 
     binmode STDOUT, ":utf8";
     binmode STDERR, ":utf8";
 
-    my $csv = Text::CSV->new ({ binary => 1, auto_diag => 1 });
+    my $csv = Text::CSV->new( { binary => 1, auto_diag => 1 } );
     open my $fh, "<:encoding(utf8)", "$args[0]" or die "$args[0]: $!";
-    my $headers = $csv->getline($fh);
-    map { $_ = trim($_) } @$headers;
-    $csv->column_names(@$headers);
+
+    my @headers = map { s/(^\s+|\s+$)//g; $_ } @{ $csv->getline($fh) };
+    $csv->column_names(@headers);
 
     # , 코드번호, , 성별, 복종, 번호, 가슴둘레, 팔길이, 허리둘레, 바지기장, 밑단 (인치), 색상, 패턴, 계절, 상의허리, 어깨, 소매통, 상의총길이, 엉덩이둘레, 허벅지둘레, 밑위, , , 기증자, 기증일시, 기증벌수, 연락처, 이메일, 주소, SNS, 기증벌수, 기증내역, 기증메세지, 경험공유여부,
 
@@ -55,7 +61,7 @@ sub run {
     }
 
     my $loop = 1;
-    my ($success, $fail) = (0, 0);
+    my ( $success, $fail ) = ( 0, 0 );
     while (my $row = $csv->getline_hr($fh)) {
         $loop++;
         my $category_id;
@@ -74,7 +80,7 @@ sub run {
         }
 
         my $res = $ua->request(
-            POST 'http://localhost:5000/clothes.json',
+            POST $opt->url . '/clothes.json',
             [
                 chest       => $row->{'가슴둘레'},
                 waist       => $row->{'허리둘레'},
@@ -94,35 +100,8 @@ sub run {
         }
     }
 
+    close $fh;
+
     say "SUCCESS: $success";
     say "FAIL   : $fail";
 }
-
-__END__
-
-=pod
-
-=encoding utf-8
-
-=head1 NAME
-
-import.pl - import clothes data from csv to database
-
-=head1 SYNOPSIS
-
-    $ import.pl [OPTIONS] /path/to/clothe-data.csv
-
-    OPTIONS
-      -h, --help          show this messages
-
-=head1 DESCRIPTION
-
-=head1 LICENSE
-
-same as Perl.
-
-=head1 AUTHOR
-
-Hyungsuk Hong
-
-=cut
