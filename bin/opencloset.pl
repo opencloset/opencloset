0227746815d40a93e08d84c618964c1eac4a8ab4
Keedi Kim <keedi.k@gmail.com>
2013-11-29 18:26:12 +0900

    api: implement get order api

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -634,6 +634,43 @@ group {
 
     sub api_get_order {
         my $self = shift;
+
+        #
+        # fetch params
+        #
+        my %params = $self->get_params(qw/ id /);
+
+        #
+        # validate params
+        #
+        my $v = $self->create_validator;
+        $v->field('id')->required(1)->regexp(qr/^\d+$/);
+        unless ( $self->validate( $v, \%params ) ) {
+            my @error_str;
+            while ( my ( $k, $v ) = each %{ $v->errors } ) {
+                push @error_str, "$k:$v";
+            }
+            return $self->error( 400, {
+                str  => join(',', @error_str),
+                data => $v->errors,
+            });
+        }
+
+        #
+        # find user
+        #
+        my $order = $DB->resultset('Order')->find( \%params );
+        return $self->error( 404, {
+            str  => 'order not found',
+            data => {},
+        }) unless $order;
+
+        #
+        # response
+        #
+        my %data = ( $order->get_columns );
+
+        $self->respond_to( json => { status => 200, json => \%data } );
     }
 
     sub api_update_order {
