4a22601f6129c12d2f5bb771ae75290f5dff1b7b
Keedi Kim <keedi.k@gmail.com>
2014-01-20 12:19:59 +0900

    api: trim clothes code when adding to order_detail
    주문서 생성시 상세 항목을 추가할 때 의류 품번
    앞자리의 0을 제거합니다.

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -465,7 +465,7 @@ helper create_order => sub {
                     my $clothes = $DB->resultset('Clothes')->find({ code => $_ });
                     $order->add_to_order_details({
                         clothes_code => $clothes->code,
-                        name         => join( q{ - }, $clothes->code, $clothes->category ),
+                        name         => join( q{ - }, $self->trim_clothes_code($clothes), $clothes->category ),
                         price        => $clothes->price,
                         final_price  => ( $clothes->price + $clothes->price * 0.2 * ($order_params->{additional_day} || 0) ),
 
