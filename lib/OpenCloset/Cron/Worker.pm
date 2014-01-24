a1bf659f3f49ed76db92d9cc68f6c48847da2999
Keedi Kim <keedi.k@gmail.com>
2014-01-24 18:00:11 +0900

    lib: Set default callback for cron worker

--- lib/OpenCloset/Cron/Worker.pm
+++ lib/OpenCloset/Cron/Worker.pm
@@ -6,9 +6,21 @@ use Moo;
 use MooX::Types::MooseLike::Base qw( CodeRef Str );
 use namespace::clean -except => 'meta';
 
+use Scalar::Util qw( weaken );
+
 has name => ( is => 'ro', isa => Str,     required => 1 );
 has cron => ( is => 'ro', isa => Str,     required => 1 );
-has cb   => ( is => 'ro', isa => CodeRef, required => 1 );
+has cb   => ( is => 'rw', isa => CodeRef, builder  => '_default_cb' );
+
+sub _default_cb {
+    my $self = shift;
+
+    weaken($self);
+    return sub {
+        my $name = $self->name;
+        AE::log( debug => "$name: dummy cron worker" );
+    };
+}
 
 has _timer => (
     is        => 'rw',
