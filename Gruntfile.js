67038b093f812e8dd9bf7643b4f6d14c2c370c38
Hyungsuk Hong <aanoaa@gmail.com>
2014-11-12 14:47:59 +0900

    add front-end package managing meta files

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      compileBare: {
+        options: {
+          bare: true,
+          join: true
+        },
+        expand: true,
+        cwd: 'public/js',
+        src: ['*.coffee'],
+        dest: 'public/js/',
+        ext: '.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + 
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'public/js',
+          src: '**/*.js',
+          dest: 'public/dist'
+        }]
+      }
+    },
+    watch: {
+      js: {
+        files: ['public/js/*.js'],
+        tasks: 'uglify'
+      },
+      sass: {
+        files: ['public/assets/sass/screen.scss'],
+        tasks: 'compass'
+      },
+      coffee: {
+        files: ['public/js/*.coffee'],
+        tasks: 'coffee'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.registerTask('default', ['coffee', 'uglify', 'compass']);
+};
---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+
+output_style = :compact
+line_comments = false
