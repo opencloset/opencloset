0a9a178ad4ba12e3ff9501e2ce4b4c1fe8f4b42c
Hyungsuk Hong <aanoaa@gmail.com>
2014-11-27 19:24:03 +0900

    user address changes to address1 .. address4
    address1: dbid
    address2: 도로명주소
    address3: 구(old)주소
    address4: 상세주소

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -1112,7 +1112,10 @@ group {
                 $user->email,
                 $user->create_date,
                 $user->user_info->phone,
-                $user->user_info->address,
+                $user->user_info->address1,
+                $user->user_info->address2,
+                $user->user_info->address3,
+                $user->user_info->address4,
                 $user->user_info->gender,
                 $user->user_info->birth,
             );
@@ -1125,7 +1128,10 @@ group {
             email
             createdate
             phone
-            address
+            address1
+            address2
+            address3
+            address4
             gender
             birth
         /);
@@ -1288,7 +1294,10 @@ group {
             update_date
         /);
         my %user_info_params = $self->get_params(qw/
-            address
+            address1
+            address2
+            address3
+            address4
             arm
             belly
             birth
@@ -1418,7 +1427,10 @@ group {
             update_date
         /);
         my %user_info_params = $self->get_params(qw/
-            address
+            address1
+            address2
+            address3
+            address4
             arm
             belly
             birth
@@ -3532,7 +3544,10 @@ any '/visit' => sub {
 
     my $email         = $self->param('email');
     my $gender        = $self->param('gender');
-    my $address       = $self->param('address');
+    my $address1      = $self->param('address1');
+    my $address2      = $self->param('address2');
+    my $address3      = $self->param('address3');
+    my $address4      = $self->param('address4');
     my $birth         = $self->param('birth');
     my $height        = $self->param('height');
     my $weight        = $self->param('weight');
@@ -3550,7 +3565,10 @@ any '/visit' => sub {
 
     app->log->debug("email: $email");
     app->log->debug("gender: $gender");
-    app->log->debug("address: $address");
+    app->log->debug("address1: $address1");
+    app->log->debug("address2: $address2");
+    app->log->debug("address3: $address3");
+    app->log->debug("address4: $address4");
     app->log->debug("birth: $birth");
     app->log->debug("height: $height");
     app->log->debug("weight: $weight");
@@ -3605,7 +3623,10 @@ any '/visit' => sub {
         $user_params{id}            = $user->id;
         $user_params{email}         = $email    if $email    && $email    ne $user->email;
         $user_info_params{gender}   = $gender   if $gender   && $gender   ne $user->user_info->gender;
-        $user_info_params{address}  = $address  if $address  && $address  ne $user->user_info->address;
+        $user_info_params{address1} = $address1 if $address1 && $address1 ne $user->user_info->address1;
+        $user_info_params{address2} = $address2 if $address2 && $address2 ne $user->user_info->address2;
+        $user_info_params{address3} = $address3 if $address3 && $address3 ne $user->user_info->address3;
+        $user_info_params{address4} = $address4 if $address4 && $address4 ne $user->user_info->address4;
         $user_info_params{birth}    = $birth    if $birth    && $birth    ne $user->user_info->birth;
         $user_info_params{height}   = $height   if $height   && $height   ne $user->user_info->height;
         $user_info_params{weight}   = $weight   if $weight   && $weight   ne $user->user_info->weight;
@@ -3722,12 +3743,12 @@ get '/user' => sub {
     my $q = $self->param('q');
     my $cond = $q
         ? [
-        { 'name'             => { like => "%$q%" } },
-        { 'email'            => { like => "%$q%" } },
-        { 'user_info.phone'  => { like => "%$q%" } },
-        { 'user_info.address'=> { like => "%$q%" } },
-        { 'user_info.birth'  => { like => "%$q%" } },
-        { 'user_info.gender' => $q },
+        { 'name'               => { like => "%$q%" } },
+        { 'email'              => { like => "%$q%" } },
+        { 'user_info.phone'    => { like => "%$q%" } },
+        { 'user_info.address4' => { like => "%$q%" } },    # 상세주소만 검색
+        { 'user_info.birth'    => { like => "%$q%" } },
+        { 'user_info.gender'   => $q },
         ]
         : {};
 
