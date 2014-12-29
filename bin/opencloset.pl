8901635dc1e6f4fbb1da9578507d1d1bb7288cec
Keedi Kim <keedi.k@gmail.com>
2014-12-29 16:12:22 +0900

    Set status as '결제대기(19)' for part-returned order

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -1896,13 +1896,13 @@ group {
         $order_params{target_date}      = $order->target_date;
         $order_params{user_id}          = $order->user_id;
         $order_params{user_target_date} = $order->user_target_date;
+        $order_params{status_id}        = 19; # 결제대기
 
         delete $order_params{id};
         delete $order_params{late_fee_pay_with};
         delete $order_params{price_pay_with};
         delete $order_params{return_date};
         delete $order_params{return_method};
-        delete $order_params{status_id};
 
         my $new_order;
         {
