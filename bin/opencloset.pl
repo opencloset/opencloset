f0b7d5a81b1b607b9b42582a4be694d00414874b
Keedi Kim <keedi.k@gmail.com>
2015-01-12 22:02:35 +0900

    Set status as '결제대기' when packaging
    포장시 의류와 모든 주문서 하부 내역을 결제대기 상태로 변경합니다.

--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -4887,8 +4887,14 @@ post '/order' => sub {
                     app->config->{category}{ $clothes->category }{str},
                 );
 
+                #
+                # 주문서 하부의 모든 의류 항목을 결제대기(19) 상태로 변경
+                #
+                $clothes->update({ status_id => 19 });
+
                 $order->add_to_order_details({
                     clothes_code => $clothes->code,
+                    status_id    => 19, # 주문서 하부의 모든 의류 항목을 결제대기(19) 상태로 변경
                     name         => $name,
                     price        => $clothes->price,
                     final_price  => $clothes->price,
