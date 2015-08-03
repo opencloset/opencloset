351282df5acbbd6ab73acb9e6c09ac559739980c
Keedi Kim <keedi.k@gmail.com>
2015-08-03 14:16:40 +0900

    Apply new module api usage to script

--- bin/opencloset-size-guess.pl
+++ bin/opencloset-size-guess.pl
@@ -4,45 +4,77 @@ use utf8;
 use strict;
 use warnings;
 
-use OpenCloset::Schema;
-use OpenCloset::Size::Guess::Local;
-use OpenCloset::Size::Guess::BodyKit;
+use DDP;
+use FindBin qw( $Bin $Script );
+
 use OpenCloset::Config;
+use OpenCloset::Schema;
+use OpenCloset::Size::Guess;
 
 binmode STDOUT, ':utf8';
+binmode STDERR, ':utf8';
 
-my $gender = shift;
-my $height = shift;
-my $weight = shift;
-die "Usage: $0 <gender> <height> <weight>\n"
-    unless $gender
-    and $height
-    and $weight;
+my $config_file = shift || "$Bin/../app.conf";
+my $gender      = shift;
+my $height      = shift;
+my $weight      = shift;
 
-my $CONF = OpenCloset::Config::load( $ENV{MOJO_CONFIG} || 'app.conf' );
-my $DB = OpenCloset::Schema->connect(
-    {
-        dsn      => $CONF->{database}{dsn},
-        user     => $CONF->{database}{user},
-        password => $CONF->{database}{pass},
-        %{ $CONF->{database}{opts} },
-    }
-);
+die "Usage: $Script <config path> <gender> <height> <weight>\n"
+    unless ( $config_file
+    && -f $config_file
+    && $gender
+    && $gender =~ m/^(male|female)$/
+    && $height
+    && $weight );
 
-my $local = OpenCloset::Size::Guess::Local->new(
-    schema => $DB,
-    gender => $gender,
-    height => $height,
-    weight => $weight
-);
+my $CONF = OpenCloset::Config::load($config_file);
+die "$config_file: cannot load config\n" unless $CONF;
 
-my $bodykit = OpenCloset::Size::Guess::BodyKit->new(
-    access_key => $CONF->{bodykit}{access_key},
-    secret     => $CONF->{bodykit}{secret},
-    gender     => $gender,
-    height     => $height,
-    weight     => $weight
-);
+{
+    my $DB_CONF  = $CONF->{database};
+    my $TIMEZONE = $CONF->{timezone};
 
-print "$local\n";
-print "$bodykit\n";
+    die "$config_file: database is needed\n" unless $DB_CONF;
+    die "$config_file: timezone is needed\n" unless $TIMEZONE;
+
+    my $DB = OpenCloset::Schema->connect(
+        {
+            dsn      => $DB_CONF->{dsn},
+            user     => $DB_CONF->{user},
+            password => $DB_CONF->{pass},
+            %{ $DB_CONF->{opts} },
+        }
+    );
+
+    # Create a guesser
+    my $guesser = OpenCloset::Size::Guess->new(
+        'DB',
+        height     => $height,
+        weight     => $weight,
+        gender     => $gender,
+        _time_zone => $TIMEZONE,
+        _schema    => $DB,
+    );
+
+    my $info = $guesser->guess;
+    p $info;
+}
+
+{
+    my $BODYKIT = $CONF->{bodykit};
+
+    die "$config_file: bodykit is needed\n" unless $BODYKIT;
+
+    # Create a guesser
+    my $guesser = OpenCloset::Size::Guess->new(
+        'BodyKit',
+        height     => $height,
+        weight     => $weight,
+        gender     => $gender,
+        _accessKey => $BODYKIT->{accessKey},
+        _secret    => $BODYKIT->{secret},
+    );
+
+    my $info = $guesser->guess;
+    p $info;
+}
