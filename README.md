98d472613a005ab48ae43f1e7a07d0a8f80e5008
Hyungsuk Hong <aanoaa@gmail.com>
2016-03-03 12:29:12 +0900

    Bumpup version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ opencloset
 
 ## Version ##
 
-v1.0.5
+v1.0.6
 
 ## Requirements ##
 
--- bin/opencloset.pl
+++ bin/opencloset.pl
@@ -59,7 +59,7 @@ use Postcodify;
 use OpenCloset::Schema;
 use OpenCloset::Size::Guess;
 
-use version; our $VERSION = version->declare("v1.0.5");
+use version; our $VERSION = version->declare("v1.0.6");
 
 app->defaults(
     %{ plugin 'Config' => {
