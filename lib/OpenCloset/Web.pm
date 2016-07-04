8f33cccad88f7ce0656fb706c4b3e876c6c3aa09
Hyungsuk Hong <aanoaa@gmail.com>
2016-07-04 14:23:44 +0900

    Bumpup version

--- lib/OpenCloset/Web.pm
+++ lib/OpenCloset/Web.pm
@@ -5,7 +5,7 @@ use CHI;
 use DateTime;
 
 use OpenCloset::Schema;
-use version; our $VERSION = qv("v1.8.2");
+use version; our $VERSION = qv("v1.8.3");
 has CACHE => sub {
     my $self  = shift;
     my $cache = CHI->new(
