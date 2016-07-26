33e5f8fb373b35dd36adf3d9494de081f2e4f7a8
Keedi Kim <keedi.k@gmail.com>
2016-07-26 15:51:49 +0900

    Make cache for every clothes

--- bin/opencloset-stat-clothes-rent-cache.pl
+++ bin/opencloset-stat-clothes-rent-cache.pl
@@ -29,10 +29,11 @@ my $CONF = OpenCloset::Config::load($config_file);
 die "$config_file: cannot load config\n" unless $CONF;
 
 {
-    my $DB_CONF    = $CONF->{database};
-    my $TIMEZONE   = $CONF->{timezone};
-    my $CACHE_DIR  = $CONF->{cache}{dir};
-    my $START_DATE = $CONF->{start_date};
+    my $DB_CONF     = $CONF->{database};
+    my $TIMEZONE    = $CONF->{timezone};
+    my $CACHE_DIR   = $CONF->{cache}{dir};
+    my $START_DATE  = $CONF->{start_date};
+    my $CATEGORY_OF = $CONF->{category};
 
     die "$config_file: database is needed\n"   unless $DB_CONF;
     die "$config_file: timezone is needed\n"   unless $TIMEZONE;
@@ -71,25 +72,27 @@ die "$config_file: cannot load config\n" unless $CONF;
     };
     die "cannot create datetime object: start_date\n" unless $start_date;
 
+    my @category_list = keys %$CATEGORY_OF;
+
     my %clothes_list;
     my $clothes_rs = $DB->resultset("Clothes")->search(
         {
             gender    => [qw/ male female /],
-            category  => [qw/ jacket pants skirt /],
-            status_id => [                          # 가용 가능 의류 상태
-                1,                                  # 대여가능
-                2,                                  # 대여중
-                3,                                  # 대여불가
-                4,                                  # 예약
-                5,                                  # 세탁
-                6,                                  # 수선
-                9,                                  # 반납
-                10,                                 # 부분반납
-                11,                                 # 반납배송중
-                16,                                 # 치수측정
-                17,                                 # 의류준비
-                18,                                 # 포장
-                19,                                 # 결제대기
+            category  => \@category_list,
+            status_id => [                   # 가용 가능 의류 상태
+                1,                           # 대여가능
+                2,                           # 대여중
+                3,                           # 대여불가
+                4,                           # 예약
+                5,                           # 세탁
+                6,                           # 수선
+                9,                           # 반납
+                10,                          # 부분반납
+                11,                          # 반납배송중
+                16,                          # 치수측정
+                17,                          # 의류준비
+                18,                          # 포장
+                19,                          # 결제대기
             ],
         },
         {},
@@ -110,7 +113,7 @@ die "$config_file: cannot load config\n" unless $CONF;
     }
 
     for my $gender (qw/ male female /) {
-        for my $category (qw/ jacket pants skirt /) {
+        for my $category (@category_list) {
             my $data = $clothes_list{$gender}{$category};
             next unless $data && ref($data) eq "ARRAY";
 
