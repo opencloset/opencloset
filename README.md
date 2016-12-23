ef5b29ca50b9fd9aa281543c92a6734b3a0f5004
Keedi Kim <keedi.k@gmail.com>
2016-12-23 17:33:21 +0900

    Bump up version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ opencloset
 
 ## Version ##
 
-v1.8.36
+v1.8.37
 
 ## Requirements ##
 
--- lib/OpenCloset/Web.pm
+++ lib/OpenCloset/Web.pm
@@ -5,7 +5,7 @@ use CHI;
 use DateTime;
 
 use OpenCloset::Schema;
-use version; our $VERSION = qv("v1.8.36");
+use version; our $VERSION = qv("v1.8.37");
 has CACHE => sub {
     my $self  = shift;
     my $cache = CHI->new(
