101f3c3d1f5ef6e81d8bfbd76a2284b8b6b66d55
Hyungsuk Hong <aanoaa@gmail.com>
2017-08-01 16:04:14 +0900

    Bump up version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ opencloset
 
 ## Version ##
 
-v1.10.5
+v1.10.6
 
 ## Requirements ##
 
--- lib/OpenCloset/Web.pm
+++ lib/OpenCloset/Web.pm
@@ -1,7 +1,7 @@
 package OpenCloset::Web;
 use Mojo::Base 'Mojolicious';
 
-use version; our $VERSION = qv("v1.10.5");
+use version; our $VERSION = qv("v1.10.6");
 
 use CHI;
 use DateTime;
